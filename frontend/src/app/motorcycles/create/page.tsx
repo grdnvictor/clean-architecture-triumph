ade7e6f43bd7659bda510d84a67a740a07228144
AlexandreAllard <alrdalexandre@gmail.com>
2025-02-09 10:42:33 +0100

    WIP FRONT MOTORCYCLES

--- frontend/src/app/motorcycles/create/page.tsx
+++ frontend/src/app/motorcycles/create/page.tsx
@@ -1,9 +1,79 @@
 "use client";
 
-export default function MotorcyclesCreationPage() {
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function CreateMotorcyclePage() {
+    const [formData, setFormData] = useState({
+        currentMileage: "",
+        vin: "",
+        modelId: "",
+        concessionId: ""
+    });
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        try {
+            const response = await fetch("http://localhost:8000/motorcycles", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(formData)
+            });
+
+            if (!response.ok) {
+                throw new Error("Erreur lors de la création du client");
+            }
+
+            router.push("/motorcycles");
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
-        <div className="container mx-auto p-6">
-            <h1 className="text-3xl font-bold mb-6">Création de client</h1>
+        <div className="container mx-auto p-6 grid place-items-center h-full w-full">
+            <Card className={"mx-auto w-[400px]"}>
+                <CardHeader>
+                    <CardTitle>Créer une moto</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={handleSubmit} className="space-y-4">
+                        <div>
+                            <Label htmlFor="firstName">Current Mileage</Label>
+                            <Input id="currentMileage" name="currentMileage" value={formData.currentMileage} onChange={handleChange} required />
+                        </div>
+                        <div>
+                            <Label htmlFor="vin">VIN</Label>
+                            <Input id="vin" name="vin" value={formData.vin} onChange={handleChange} required />
+                        </div>
+                        <div>
+                            <Label htmlFor="modelId">Model ID</Label>
+                            <Input id="modelId" name="modelId" value={formData.modelId} onChange={handleChange} required />
+                        </div>
+                        <div>
+                            <Label htmlFor="concessionId">ID Concession</Label>
+                            <Input id="concessionId" name="concessionId" value={formData.concessionId} onChange={handleChange} required />
+                        </div>
+                        <Button type="submit" disabled={loading}>
+                            {loading ? "Ajout en cours..." : "Ajouter"}
+                        </Button>
+                    </form>
+                </CardContent>
+            </Card>
         </div>
     );
 }
--- frontend/src/app/motorcycles/page.tsx
+++ frontend/src/app/motorcycles/page.tsx
@@ -10,6 +10,8 @@ import {
     TableCell,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Trash } from "lucide-react";
 
 export default function MotorcyclesPage() {
     const [motorcycles, setMotorcycles] = useState([]);
@@ -19,15 +21,29 @@ export default function MotorcyclesPage() {
             .then((response) => response.json())
             .then((data) => setMotorcycles(data))
             .catch((error) =>
-                console.error("Erreur lors de la récupération des motos :", error)
+                console.error("Erreur lors de la récupération des clients :", error)
             );
     }, []);
 
+    const handleDelete = async (id) => {
+        try {
+            const response = await fetch(`http://localhost:8000/motorcycles/${id}`, {
+                method: "DELETE",
+            });
+            if (!response.ok) {
+                throw new Error("Erreur lors de la suppression du client");
+            }
+            setMotorcycles((motorcycles) =>
+                motorcycles.filter((motorcycle) => motorcycle.id !== id)
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="container mx-auto p-6">
-            <h1 className="text-3xl font-bold mb-6">Liste des Motos</h1>
-
-            {/* Wrapper pour le tableau en full width avec scroll horizontal si nécessaire */}
+            <h1 className="text-3xl font-bold mb-6">Liste des motos</h1>
             <div className="w-full overflow-x-auto">
                 <Table className="w-full">
                     <TableHeader>
@@ -35,34 +51,40 @@ export default function MotorcyclesPage() {
                             <TableHead>ID</TableHead>
                             <TableHead>VIN</TableHead>
                             <TableHead>Model ID</TableHead>
-                            <TableHead>Concession ID</TableHead>
-                            <TableHead>Kilométrage</TableHead>
-                            <TableHead>Créé le</TableHead>
-                            <TableHead>Mis à jour le</TableHead>
+                            <TableHead>Current Mileage</TableHead>
+                            <TableHead>Créée le</TableHead>
+                            <TableHead>Mise à jour le</TableHead>
+                            <TableHead>Actions</TableHead>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        {motorcycles.map((moto) => (
-                            <TableRow key={moto.id}>
-                                <TableCell>{moto.id}</TableCell>
-                                <TableCell>{moto.vin}</TableCell>
-                                <TableCell>{moto.modelid}</TableCell>
-                                <TableCell>{moto.concessionid}</TableCell>
-                                <TableCell>{moto.currentmileage}</TableCell>
+                        {motorcycles.map((client) => (
+                            <TableRow key={client.id}>
+                                <TableCell>{client.id}</TableCell>
+                                <TableCell>{client.vin}</TableCell>
+                                <TableCell>{client.modelid}</TableCell>
+                                <TableCell>{client.concessionid}</TableCell>
                                 <TableCell>
-                                    {new Date(moto.createdat).toLocaleDateString()}
+                                    {new Date(client.createdat).toLocaleDateString()}
                                 </TableCell>
                                 <TableCell>
-                                    {new Date(moto.updatedat).toLocaleDateString()}
+                                    {new Date(client.updatedat).toLocaleDateString()}
+                                </TableCell>
+                                <TableCell>
+                                    <Trash
+                                        onClick={() => handleDelete(client.id)}
+                                        className="text-red-500 w-4 h-4 cursor-pointer"
+                                    />
                                 </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </div>
-
             <div className="mt-8">
-                <Button>Ajouter une moto</Button>
+                <Button>
+                    <Link href="/motorcycles/create">Ajouter une moto</Link>
+                </Button>
             </div>
         </div>
     );
